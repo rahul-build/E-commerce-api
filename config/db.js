589725af5f384ad3d71474db5c202e8424cb674f
rahul-build <rahulbuild.dev@gmail.com>
2025-07-03 10:41:54 +0530

    feat:Database is connected

---
+++ config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import colors from "colors";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log(`Mongodb Connected ${mongoose.connection.host}`);
+  } catch (error) {
+    console.log(`Mondobd error ${error}`.bgWhite);
+  }
+};
+export default connectDB;
--- server.js
+++ server.js
@@ -2,8 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import colors from "colors";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
